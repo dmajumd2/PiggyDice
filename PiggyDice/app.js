c0591d9c9a3fe5343ba1a6bfa21d03440c6a540b
Debashish <dmajumd2@binghamton.edu>
2018-01-05 14:36:58 -0500

    Added a state variable to disable the button

--- PiggyDice/app.js
+++ PiggyDice/app.js
@@ -1,65 +1,56 @@
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
-
-//document.querySelector("#current-" + activePlayer).textContent = dice;
-
-var ad = document.querySelector('#score-1').textContent;
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+init();
 
 
 //On click event on clicking the roll dice button and displaying the random number
 document.querySelector('.btn-roll').addEventListener('click', function(){
-	
-	//1. Random number
-	var dice = Math.floor(Math.random() * 6) + 1;
-	
-	//2. Display the result
-	var diceDom = document.querySelector('.dice');
-	diceDom.style.display = 'block';
-	diceDom.src = 'dice-' + dice + '.png';
-	
-	//3. Update the round score if the rolled number was not 1
-	if(dice !== 1){
-		// Add score
-		roundScore += dice;
-		document.querySelector('#current-' + activePlayer).textContent = roundScore;
-	} else {
-		//Next player
-		nextPlayer();
-	}
-	
-});
+	if(gamePlaying){
+					//1. Random number
+					var dice = Math.floor(Math.random() * 6) + 1;
 
+					//2. Display the result
+					var diceDom = document.querySelector('.dice');
+					diceDom.style.display = 'block';
+					diceDom.src = 'dice-' + dice + '.png';
+
+					//3. Update the round score if the rolled number was not 1
+					if(dice !== 1){
+					// Add score
+					roundScore += dice;
+					document.querySelector('#current-' + activePlayer).textContent = roundScore;
+					} else {
+					//Next player
+					nextPlayer();
+					}
+			}
+
+	});
+
+
+// on click event on hold button
 document.querySelector('.btn-hold').addEventListener('click', function(){
-	//1. Add current score to global score
-	 scores[activePlayer] += roundScore;
 	
-	//2. Update the UI
-	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+	if(gamePlaying){
+			//1. Add current score to global score
+	 		scores[activePlayer] += roundScore;
 	
-	//3. check if player won the game
-	if(scores[activePlayer] >= 20){
-		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-		document.querySelector('.dice').style.display = 'none';
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active ');
-	} else {
-		nextPlayer();
-	}
+			//2. Update the UI
+			document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 	
-})
+			//3. check if player won the game
+			if(scores[activePlayer] >= 100){
+				document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+				document.querySelector('.dice').style.display = 'none';
+				document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+				document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+				gamePlaying = false;
+				} else {
+						nextPlayer();
+						}
+		}
+	
+	})
 
 
 function nextPlayer(){
@@ -85,6 +76,7 @@ function init(){
 		scores = [0, 0];
 		roundScore = 0;
 		activePlayer = 0;
+		gamePlaying = true;
 	
 	
 		document.querySelector('.dice').style.display = 'none';
